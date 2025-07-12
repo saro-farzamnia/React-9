7df94d0050d3929fef7bcd4edfb2509efd6b9546
saro-farzamnia <sarofarzamnia0@gmail.com>
2025-07-11 20:08:11 -0700

    eseReducer

--- React-9/src/App.jsx
+++ React-9/src/App.jsx
@@ -1,45 +1,12 @@
 
-import { useReducer, useState } from 'react'
 import './App.css'
-
-const reducer = (state,action)=>{
-  console.log({state,action})
-  switch (action){
-    case "INCEAS":
-      return state + 1;
-      case "DESCREAS":
-        return state -1;
-      case "RESET":
-        return 0;
-
-    default:
-      throw new Error ("Click button")
-  }
-}
+import Reducer from './Reducer'
 
 function App() {
-  const [number,dispatch]=useReducer(reducer,0);
-  const [error,setError]=useState();
-
-  const increasHandler = ()=>{
-    dispatch("INCEAS")
-  }
-  const resetHandler = ()=>{
-    dispatch("RESET")
-  }
-  const descreasHandler = ()=>{
-    dispatch("DESCREAS")
-  }
+  
   return (
     <>
-    <h1>Reducer</h1>
-    <h4>{number}</h4>
-    <button onClick={increasHandler}>Increas</button>
-    <button onClick={resetHandler}>Reset</button>
-    <button onClick={descreasHandler}>Descreas</button>
-    {
-      error && <h3>Click button</h3>
-    }
+    <Reducer/>
     </>
   )
 }
---
+++ React-9/src/Reducer.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useReducer, useState,  } from 'react'
+
+const reducer = (state,action)=>{
+
+  console.log({state,action})
+  switch (action){
+    case "INCEAS":
+      return state + 1;
+      case "DSCREAS":
+        return state -1;
+      case "RESET":
+        return 0;
+
+    default:
+        throw new Error ("Click button")
+  }
+
+}
+
+const Reducer = () => {
+    const [number,dispatch]=useReducer(reducer,0);
+    const [error,setError]=useState(null);
+useEffect(()=>{
+    const ere =
+    try {
+        reducer
+    } catch (error) {
+        setError(error)
+    }
+ere()
+},[])
+    const increasHandler = ()=>{
+        dispatch("INCEAS")
+    }
+    const resetHandler = ()=>{
+        dispatch("RESET")
+    }
+    const descreasHandler = ()=>{
+        dispatch("DESCREAS")
+    }
+  return (
+    <div>
+        <h1>Reducer</h1>
+        <h4>{number}</h4>
+        <button onClick={increasHandler}>Increas</button>
+        <button onClick={resetHandler}>Reset</button>
+        <button onClick={descreasHandler}>Descreas</button>
+        {
+            error && <h3>{error}</h3>
+        }
+        
+    </div>
+  )
+}
+
+export default Reducer
